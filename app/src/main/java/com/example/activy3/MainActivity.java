bf0240294c5be3bbe6881992c313a4007838ca88
RivkyRiyantoro <sirivkyriyantoro@gmail.com>
2021-04-01 16:08:36 +0700

    Activty(4) aplikasi android dengan menggunakan menu dan floating button

--- app/src/main/java/com/example/activy3/MainActivity.java
+++ app/src/main/java/com/example/activy3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activy3;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
     EditText edEmail, edPassword;
 
     String nama, password;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(),ActivtyDaftar.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
